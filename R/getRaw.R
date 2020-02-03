3be839a9459fd01841432c30c24d4f651f6f2060
Micromet <met-micro@lists.reading.ac.uk>
2020-02-03 13:41:18 +0000

    find the data

--- R/getRaw.R
+++ R/getRaw.R
@@ -1,36 +1,65 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-#knglkltgregwg
+# read in raw data and get a data frame/ plots
+library(stringr)
+
+# initial settings
+standardFileDir <- TRUE
+
+dataDir <- file.path(Sys.getenv('MM_DAILYDATA'), 'RAW', '%Y', 'London',
+                     '%SITE', '%INSTRUMENT', '%m')
+
+startDate = as.POSIXct('2020-025', format = '%Y-%j')
+endDate = as.POSIXct('2020-026', format = '%Y-%j')
 
 
-startDate <- as.POSIXct('2020-01-28')
-startDate <- as.POSIXct('2020-01-28')
+fileTimeRes = '10Hz'
+fileSpan <- 'Hourly'
+variables = c('u', 'v')
+variableColNos <- c(4, 5)
+nTimeCols <- 3
+timeColFormat <- c('%j', '%H%M', '%OS')
+# instrument -> name, level, site
+instrument <- list(name = 'Gill', site = 'URAO')
 
-li_mmdir <- '/storage/basic/micromet/Tier_raw/RAW/2020/London/IMU/LI7500_Hrly/01'
-li_keepcols <- c(1,2,3,4,5,6, 7)
-li_headers <- c('DOY', 'time', 'secs', 'CO2', 'H2O', 'CO2_absorp', 'H2O_absorp')
-li_plottingVars <- c('CO2', 'H2O', 'CO2_absorp', 'H2O_absorp')
 
-gill_mmdir <- '/storage/basic/micromet/Tier_raw/RAW/2020/London/URAO/Gill_Hrly/01'
-gill_keepcols <- c(1,2,3,4,7)
-gill_headers <- c('DOY', 'time', 'secs', 'U', 'T')
-gill_plottingVars <- c('U', 'T')
+createDateList <- function(startDate, endDate){
+  # get a vector of all dates to be gotten
+  dateList <- seq(startDate, endDate, by = 'day')
+  return(dateList)
+}
 
-mmLicorPlot<- getMMdata(li_mmdir, li_keepcols, li_headers, li_plottingVars, theDate)
-mmGillPlot <- getMMdata(gill_mmdir, gill_keepcols, gill_headers, gill_plottingVars, theDate)
+dateList <- createDateList(startDate, endDate)
 
+createReplacementVec <- function(DATE, instrument){
+  #the placeholders in the path need to be replaced with actual information
+  replacementVec <- c()
+  replacementVec <- c(replacementList, '%Y' = strftime(DATE, '%Y'))
+  replacementVec <- c(replacementVec, '%j' = strftime(DATE, '%j'))
+  replacementVec <- c(replacementVec, '%d' = strftime(DATE, '%d'))
+  replacementVec <- c(replacementVec, '%m' = strftime(DATE, '%m'))
+
+  replacementVec <- c(replacementVec, '%INSTRUMENT' = instrument$name)
+  replacementVec <- c(replacementVec, '%SITE' = instrument$site)
+
+  return(replacementVec)
+}
+
+createReplacedPath <- function(dateList, instrument, dataDir){
+
+  for (idate in 1:length(dateList)){
+    replacementList <- c()
+    replacementList <- c(replacementList, '%Y' = strftime(dateList[idate], '%Y'))
+    replacementList <- c(replacementList, '%j' = strftime(dateList[idate], '%j'))
+    replacementList <- c(replacementList, '%d' = strftime(dateList[idate], '%d'))
+    replacementList <- c(replacementList, '%m' = strftime(dateList[idate], '%m'))
+
+    replacementList <- c(replacementList, '%INSTRUMENT' = instrument$name)
+    replacementList <- c(replacementList, '%SITE' = instrument$site)
+
+
+    stringr::str_replace_all(dataDir, replacementList)
+
+   }
+}
 getMMdata <- function(mmdir, keepcols, headers, plottingVars, theDate){
   mmRawFiles <- list.files(mmdir)
   todaysMMfiles <- mmRawFiles[grepl(strftime(theDate, '%y%j'), mmRawFiles)]
