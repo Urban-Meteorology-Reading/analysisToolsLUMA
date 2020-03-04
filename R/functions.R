8f62a306208b5772e14d4ac45d19116c2077a1e7
Micromet <met-micro@lists.reading.ac.uk>
2020-03-04 09:53:48 +0000

    Fix file res bug when is na

--- R/functions.R
+++ R/functions.R
@@ -6,14 +6,15 @@ createDateList <- function(startDate, endDate){
 
 getFileRes <- function(fileTimeRes){
   #get the file res as a number and a unit
-  fileResList = list()
-  fileResList[['Res']] <- readr::parse_number(fileTimeRes)
-  fileResList[['Unit']] <- str_remove(fileTimeRes, as.character(fileResList[['Res']]))
-  print(fileResList)
-  # check if it's one of the valid units
-  if (fileResList$Unit != 'min' &  fileResList$Unit != 'sec' &
-      fileResList$Unit != 'Hz'){
-    stop(paste('Invalid fileTimeRes unit:', fileResList$Unit, 'valid units are: "min", "sec" and "Hz"'))
+  if (!is.na(fileTimeRes)) {
+    fileResList = list()
+    fileResList[['Res']] <- readr::parse_number(fileTimeRes)
+    fileResList[['Unit']] <- str_remove(fileTimeRes, as.character(fileResList[['Res']]))
+    # check if it's one of the valid units
+    if (fileResList$Unit != 'min' &  fileResList$Unit != 'sec' &
+        fileResList$Unit != 'Hz'){
+      stop(paste('Invalid fileTimeRes unit:', fileResList$Unit, 'valid units are: "min", "sec" and "Hz"'))
+    }
   }
   return(fileResList)
 }
